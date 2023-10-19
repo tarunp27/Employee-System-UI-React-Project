49dd0e005c0a75fc6c17680613a87b7b98dbcb86
Tarun Pugalendi <104558132+tarunp27@users.noreply.github.com>
2023-10-19 21:00:11 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddEmp from "./components/AddEmp";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import EditEmp from "./components/EditEmp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/addEmp" element={<AddEmp />}></Route>
+        <Route path="/editEmp/:id" element={<EditEmp />}></Route>
+      </Routes>
     </div>
   );
 }
